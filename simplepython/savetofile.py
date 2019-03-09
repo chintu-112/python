bd1ac3bda515b3bbdbb75254255b78bc7ac41710
novicejava1 <sudhirbhoga@gmail.com>
2019-03-09 08:39:05 +0530

    save to file example

---
+++ simplepython/savetofile.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+dad = []
+kid = []
+
+try:
+    data = open('unstruct_data.txt')
+
+    for line in data:
+        try:
+            (person, line_spoken) = line.split(':')
+            line_spoken = line_spoken.strip()
+            if person == 'Dad ':
+                dad.append(line_spoken)
+            elif person == 'Kid ':
+                kid.append(line_spoken)
+            else:
+                pass
+        except ValueError:
+            pass
+    data.close()
+except IOError:
+    print('The data file is missing')
+
+print(dad)
+print(kid)
+
+### Save the data and kid list to a file
+try:
+    dadfile = open('dadfile.txt', 'w')
+    kidfile = open('kidfile.txt', 'w')
+
+    print(dad, file=dadfile)
+    print(kid, file=kidfile)
+
+    #dadfile.close()
+    #kidfile.close()
+except IOError:
+    print('File Error')
+finally:
+    dadfile.close()
+    kidfile.close()
+
+
+
+
+            
+
